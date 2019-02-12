482e08eef29b2c2f33f442b27a595b1be402e2f2
Betty Zhou <betty.zhou@alumni.ubc.ca>
2019-02-11 23:59:27 -0800

    reorganized package repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='InstaF_Python',
+    version='0.1',
+    packages=['InstaF_Python',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
