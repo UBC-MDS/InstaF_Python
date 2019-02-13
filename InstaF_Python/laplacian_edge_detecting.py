430cffa2db170bab96a083b75734de6e36030e93
Reza Bagheri <reza.bagheri79@gmail.com>
2019-02-12 19:35:57 -0800

    The Lapacian edge detection function added

--- InstaF_Python/laplacian_edge_detecting.py
+++ InstaF_Python/laplacian_edge_detecting.py
@@ -1,20 +1,54 @@
 import numpy as np
-import skimage.io
+import matplotlib.pyplot as plt
 
 def laplacian_edge_detecting(input_image,  output_image):
     '''
     Perform laplacian filtering on the image to get the secondary derivitive of the image matrix, based on the defined input path
-
+    
+    The function applys a Laplacian filter to each color chanel of the input image. The filter is [[0, -1, 0],[-1,4,-1],[0,-1,0]]. Before applying the filter
+    The matrix. The filtered image will be saved on the output_image path.  
+.
     Inputs
     -----
     input_path: string, path for an image file in .png format
     output_path: string, path for the output image in .png format
 
-    Returns
-    ----------
-    the current return is a matrix, which is the image matrix after filtering, just to make the test easier. In the future, the output will be an Image
+    
 
     '''
-    converted_matrix = plt.imread(input_image)
+    # Reading the image matrix
+    image = plt.imread(input_image)
+    
+    for k in range(3):
 
-    return converted_matrix
+        # Separating the color chanels
+        chanel = image[:,:,k]
+        
+        # Padding the matrix
+        chanel = np.pad(chanel, [(1, 1), (1, 1)], mode='edge')
+        result = np.zeros(chanel.shape)
+        
+        # Applying the convolution filter
+        for i in range(1,chanel.shape[0]-1):
+            for j in range(1,chanel.shape[1]-1):
+                result[i,j] = 4*chanel[i,j]-chanel[i+1,j]-chanel[i-1,j]-chanel[i,j+1]-chanel[i,j-1]
+                      
+        # Removing non-valied colors
+        result[result<0]=0
+        result[result>255]=255
+        
+        # Removing the pads
+        result=np.delete(result, result.shape[0]-1, 0)
+        result=np.delete(result, 0, 0)
+        result=np.delete(result, result.shape[1]-1, 1)
+        result=np.delete(result, 0, 1)
+        
+        # Copying the result
+        image[:,:,k] = result
+    
+    # Saving the image at output_path
+    plt.imsave(output_image, image)
+
+
+    
+
