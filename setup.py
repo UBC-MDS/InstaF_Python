b57de6190c1f4dda596d460e5b04e26fa903086c
Linyang Yu <42875905+lyyu0413@users.noreply.github.com>
2019-02-16 12:24:52 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='InstaF_Python',
-    version='0.1',
+    version='2.1',
     packages=['InstaF_Python',],
     license='MIT',
     long_description=open('README.txt').read(),
