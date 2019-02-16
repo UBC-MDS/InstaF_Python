31b2c9ee4cd4cd75053272b3ad886ebc991d5db8
Reza Bagheri <reza.bagheri79@gmail.com>
2019-02-15 19:08:22 -0800

    Test errors fixed

--- InstaF_Python/test/test_Laplacian_edge_detecting.py
+++ InstaF_Python/test/test_Laplacian_edge_detecting.py
@@ -5,15 +5,15 @@
 
 import numpy as np
 import pytest
-import skimage.io
-from InstaF_Python import laplacian_filter
+from InstaF_Python.laplacian_edge_detecting import laplacian_edge_detecting
+import skimage
 
 # test input: colour image
 test_img_laplacian_input = np.array([[[12, 79, 15],[171, 55, 63],[48, 90, 14]],
                                     [[36, 72, 80],[ 96, 144, 28],[215, 36, 40]],
                                     [[92, 32, 168],[144, 216, 209],[112, 54, 60]]], dtype = "uint8")
 
-skimage.io.imsave("test_py/test_image/test_img_laplacian_input.png", test_img_laplacian_input)
+skimage.io.imsave("InstaF_Python/test/test_image/test_img_laplacian_input.png", test_img_laplacian_input)
 # test output: Image with the Laplacian filter applied on it
 # filter is [[0,-1,	0],[-1,4,-1],[0,-1,	0]] and boundary is symm
 
@@ -21,30 +21,30 @@ test_img_laplacian_ex_output = np.array([[[  0,  31,   0], [255,   0, 132], [  0
                                          [[  0,   0,  29], [  0, 197,   0], [255,   0,  18]],
                                          [[  4,   0,  47], [132, 255, 255], [  0,   0,   0]]], dtype="uint8")
 
-skimage.io.imsave("test_py/test_image/test_img_laplacian_exp_output.png", test_img_laplacian_ex_output)
+skimage.io.imsave("InstaF_Python/test/test_image/test_img_laplacian_exp_output.png", test_img_laplacian_ex_output)
 
 # Check whether laplacian_filter function is working properly
 def test_laplacian():
-    laplacian_filter("test_py/test_image/test_img_laplacian_input.png", "test_py/test_image/laplacian_output.png")
-    output = skimage.io.imread("test_py/test_image/laplacian_output.png")
-    expected_output = skimage.io.imread("test_py/test_image/test_img_laplacian_exp_output.png")
+    laplacian_edge_detecting("InstaF_Python/test/test_image/test_img_laplacian_input.png", "InstaF_Python/test/test_image/laplacian_output.png")
+    output = skimage.io.imread("InstaF_Python/test//test_image/laplacian_output.png")
+    expected_output = skimage.io.imread("InstaF_Python/test//test_image/test_img_laplacian_exp_output.png")
     assert np.array_equal(output, expected_output), "Laplacian filter is not working properly"
 
 #Handling the exceptions with laplacian_filter()
 def test_bad_input():
     with pytest.raises(AttributeError):
-        laplacian_filter(1234, "test_py/test_image/laplacian_output.png")
+        laplacian_edge_detecting(1234, "InstaF_Python/test/test_image/laplacian_output.png")
 
 def test_input_file_no_image():
     with pytest.raises(OSError):
-        laplacian_filter("test_py/test_image/test.txt", "test_py/test_image/laplacian_output.png")
+        laplacian_edge_detecting("InstaF_Python/test/test_image/test.txt", "InstaF_Python/test/test_image/laplacian_output.png")
 
 def test_input_path_not_exist():
     with pytest.raises(FileNotFoundError):
-        laplacian_filter("./1234/123.png", "test_py/test_image/laplacian_output.png")
+        laplacian_edge_detecting("./1234/123.png", "InstaF_Python/test/test_image/laplacian_output.png")
 
 def test_bad_output():
     with pytest.raises(AttributeError):
-        laplacian_filter("test_py/test_image/test_img_laplacian_input.png", 1234)
+        laplacian_edge_detecting("InstaF_Python/test/test_image/test_img_laplacian_input.png", 1234)
 
 
