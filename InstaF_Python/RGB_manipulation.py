9ee39cfa0f7ff562173ac13cd793db9be603a2cb
Betty Zhou <betty.zhou@alumni.ubc.ca>
2019-02-15 16:12:26 -0800

    made .gitignore file

--- InstaF_Python/RGB_manipulation.py
+++ InstaF_Python/RGB_manipulation.py
@@ -20,8 +20,21 @@ def RGB_manipulation(input_path, output_path, R = 2, G = 2, B = 2):
     .png format image at the output path
     '''
 
-    # Read in .png as np.array
-    img = skimage.io.imread(input_path)[:,:,:3]
+    # Read in .png as np.array and exception handling
+    try:
+        img = skimage.io.imread(input_path)[:,:,:3]
+    except AttributeError:
+        print("Please provide the input path as a string")
+        raise
+    except OSError:
+        print("The input is not a .png file")
+        raise
+    except FileNotFoundError:
+        print("The input path does not exist")
+        raise
+    except Exception as error:
+        paste("Error: ", error)
+        raise
 
     # construct filter based on user input of RGB weights
     filter = np.array([[[R, G, B]]])
@@ -33,5 +46,12 @@ def RGB_manipulation(input_path, output_path, R = 2, G = 2, B = 2):
     output[output > 255] = 255
     output_img = output.astype(np.uint8)
 
-    # output RGB manipulated img at output input_path
-    skimage.io.imsave(output_path, output_img)
+    # output RGB manipulated img at output input_path and exception handling
+    try:
+        skimage.io.imsave(output_path, output_img)
+    except AttributeError:
+        print("Please provide the output path as a string")
+        raise
+    except Exception as error:
+        paste("Error: ", error)
+        raise
